5e2328de0ee07417f7788a74e9e867e7065ec2f1
Tyler Liu <tyler4long@gmail.com>
2024-11-27 10:33:53 -0800

    How to watch and build

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
+  console.log("Hello, React with Bun and TypeScript!");
   return (
     <div>
       <h1>Hello, React with Bun and TypeScript!</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
